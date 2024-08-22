cc7a7c704e01b503aaf7b37da5e087b3774a3f46
Shyam Kumar P S <shyam.kumar.29.ps@gmail.com>
2024-08-23 02:53:40 +0530

    move keys to env file

--- firebase/index.js
+++ firebase/index.js
@@ -3,13 +3,13 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBkQhg_KA1nV5gzgWZF738YCrF9ltgWDjw",
-  authDomain: "next-todo-e548f.firebaseapp.com",
-  projectId: "next-todo-e548f",
-  storageBucket: "next-todo-e548f.appspot.com",
-  messagingSenderId: "829786621824",
-  appId: "1:829786621824:web:3f865ea4097338bc39a365",
-  measurementId: "G-788QPZKP73"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
